2264b836a1213fa087b9e49e06d934e636c8ea26
ginpei <ginpei@localhost>
2013-03-21 12:24:08 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hichi::Application.config.secret_token = 'cd01b25dc7c80704efb4cc3ea50e719e439293f44d06fb33720c8f0e21c24b3d77ce85f2afa460d221f16f17f793f72f682de15d9c9e32a01a824467fcd9bf41'
